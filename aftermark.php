601a7edd871064599a8e965b82cf18b527192fc3
yashwantkoyyana <41166455+yashwantkoyyana@users.noreply.github.com>
2018-11-09 11:20:18 +0530

    Add files via upload

--- aftermark.php
+++ aftermark.php
@@ -1,16 +1,46 @@
+<html>
+<style>
+ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: green;
+	padding-left:px;
+	
+}
+</style>
+<title> Retrive data</title>
+<ul>
+
+</style>
+</head>
+<div id="logo"><img src="logo.jpg" height="80px" width="200px" /></div>
+
+</ul>
+</head>
+
+<body>
+<h2>Students </h2>
+
+
 
 
 <?php
+session_start();
+$couid=$_SESSION['courseid'];
+$couid=$couid."attendance";
+//echo $couid;
 if(isset($_POST['studentslist'])){
 	//print_r($_POST['studentslist']);
 	$str="";
 if(!empty($_POST['studentslist'])){
 	
 	foreach($_POST['studentslist'] as $selected){
-	echo $selected."</br>";
+	//echo $selected."</br>";
 	$str=$selected.",".$str;
 	}
-	echo $str;
+	//echo $str;
 }
 }
 $pushstr=$str;
@@ -30,15 +60,15 @@ if ($conn->connect_error) {
 //echo "Connected successfully to database";
 
 	
-	$date=$_POST["bday"];
-$sql = "INSERT INTO dipattendance (date,presentrollnos) VALUES ('$date','$pushstr')";
+$date=$_POST["bday"];
+$sql = "INSERT INTO $couid (date,presentrollnos) VALUES ('$date','$pushstr')";
 
 
 if (mysqli_query($conn, $sql)) {
-    echo "New record created successfully";
+    echo "record created successfully";
 } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
 }
 ?> 
 
-
+</html>
